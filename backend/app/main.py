5758bfff5e026816cb3dd7e448ab8add61bfcaca
SamwtAdhikary <samwitadhikary2@gmail.com>
2025-05-10 22:34:06 +0530

    Added login and register feature in frontend

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from .api.v1.router import api_v1_router
+from fastapi.middleware.cors import CORSMiddleware
 import logging
 
 app = FastAPI(
@@ -7,6 +8,19 @@ app = FastAPI(
     version="0.1.0",
     docs_url="/docs",
     redoc_url="/redoc",
+)
+
+# CORS Setup
+origins = [
+    "http://localhost:3000"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=['*'],
+    allow_headers=["*"]
 )
 
 app.include_router(api_v1_router)
